457caef2a762a6a85783ad759b96d863ace93b7c
Javier Romero <romeropaolini@gmail.com>
2018-08-16 17:06:06 +0200

    First commit

---
+++ app.js
@@ -0,0 +1,49 @@
+'use strict'
+
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const hbs = require('hbs');
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+
+const app = express();
+
+// view engine setup
+app.set('views', __dirname + '/views');
+app.set('view engine', 'hbs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// Use partials
+hbs.registerPartials(path.join(__dirname, '/views/partials'));
+hbs.localsAsTemplateData(app);
+
+// -- 404 and error handler
+app.use((req, res, next) => {
+  res.status(404);
+  res.render('not-found');
+});
+
+app.use((err, req, res, next) => {
+  // always log the error
+  console.error('ERROR', req.method, req.path, err);
+
+  // only render if the error ocurred before sending the response
+  if (!res.headersSent) {
+    res.status(500);
+    res.render('error');
+  }
+});
+
+module.exports = app;
